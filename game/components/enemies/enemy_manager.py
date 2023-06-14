50ad0c45aee53e7f988a81720eb7b3858e5bf05b
RomanVelFran2003 <romy20velfran@gmail.com>
2023-06-14 15:34:00 -0500

    Actualización de la tarea 2
    Se agregó un tercer enemigo. Los tres enemigos tienen apariencias diferentes.

--- game/components/enemies/enemy_manager.py
+++ game/components/enemies/enemy_manager.py
@@ -1,6 +1,6 @@
 from game.components.enemies.enemy import Enemy
-
 from game.components.enemies.enemy_1 import Enemy_1
+from game.components.enemies.enemy_2 import Enemy_2
 
 class EnemyManager:
     def __init__(self):
@@ -12,17 +12,23 @@ class EnemyManager:
             enemy.update(self.enemies)
         for enemy_1 in self.enemies:
             enemy_1.update(self.enemies)
+        for enemy_2 in self.enemies:
+            enemy_2.update(self.enemies)
     
     def add_enemy(self):
-        if len(self.enemies)<5:
+        if len(self.enemies)<1:
             enemy = Enemy()
             enemy_1 = Enemy_1()
+            enemy_2 = Enemy_2()
             self.enemies.append(enemy)
             self.enemies.append(enemy_1)
+            self.enemies.append(enemy_2)
 
     def draw(self,screen):
         for enemy in self.enemies:
             enemy.draw(screen)
         for enemy_1 in self.enemies:
             enemy_1.draw(screen)
+        for enemy_2 in self.enemies:
+            enemy_2.draw(screen)
             
